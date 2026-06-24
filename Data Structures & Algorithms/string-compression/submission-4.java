70b4434e3f3d9456dff907f53d19551ece11957c
Dharun B S <129257364+0x044@users.noreply.github.com>
2026-06-24 22:05:06 +0530

    Add: string-compression - submission-4

---
+++ Data Structures & Algorithms/string-compression/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int k = 0, i = 0;
+
+        while(i < n){
+            chars[k++] = chars[i];
+            int j = i+1;
+
+            while(j < n && chars[i] == chars[j]){
+                j++;
+            }
+
+            if(j - i > 1){
+                String cnt = String.valueOf(j-i);
+                for(char c : cnt.toCharArray()){
+                    chars[k++] = c;
+                }
+            }
+            i = j;
+        }
+
+        return k;
+    }
+}
